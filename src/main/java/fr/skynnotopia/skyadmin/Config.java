70858db9ac87a1e754326cf82864e1deee5e033e
Deleranax <9z3KI8ininGZmy>
2019-11-13 19:15:33 +0100

    Configuration du Plugin.yml

---
+++ src/main/java/fr/skynnotopia/skyadmin/Config.java
@@ -0,0 +1,52 @@
+package fr.skynnotopia.skyadmin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+import static fr.skynnotopia.skyadmin.Main.logPrefix;
+
+public class Config {
+
+    private static JavaPlugin plugin;
+
+    public Config(JavaPlugin p) {
+        p.saveDefaultConfig();
+        plugin = p;
+    }
+
+    public static boolean getBoolean(String s) throws NullPointerException {
+        return plugin.getConfig().getBoolean(s);
+    }
+
+    public static int getInt(String s) throws NullPointerException {
+        return plugin.getConfig().getInt(s);
+    }
+
+    public static String getString(String s) throws NullPointerException {
+        return plugin.getConfig().getString(s);
+    }
+
+    public static java.util.List<String> getStringList(String s) throws NullPointerException {
+        return plugin.getConfig().getStringList(s);
+    }
+
+    public static void set(String s, Object o) throws NullPointerException {
+        plugin.getConfig().set(s, o);
+        plugin.saveConfig();
+    }
+
+    public static boolean reload() {
+        boolean r = true;
+        try {
+            plugin.reloadConfig();
+        } catch (Exception e) {
+            Bukkit.getLogger().log(Level.SEVERE,logPrefix + e.getMessage());
+            r = false;
+        }
+        return r;
+    }
+
+
+}
--- src/main/java/fr/skynnotopia/skyadmin/Main.java
+++ src/main/java/fr/skynnotopia/skyadmin/Main.java
@@ -1,13 +1,17 @@
 package fr.skynnotopia.skyadmin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SkyAdmin extends JavaPlugin {
+public final class Main extends JavaPlugin {
+
+    static String logPrefix = "[SkyChat] ";
+    public static String messagePrefix = ChatColor.DARK_GREEN + "[SkyChat] " + ChatColor.RESET;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
     }
 
     @Override
