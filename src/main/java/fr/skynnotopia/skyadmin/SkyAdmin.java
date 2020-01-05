d3d35a632aaa276fbebe36927a6f14b5ba46716e
Deleranax <9z3KI8ininGZmy>
2020-01-05 22:51:01 +0100

    initial comit

---
+++ src/main/java/fr/skynnotopia/skyadmin/SkyAdmin.java
@@ -0,0 +1,17 @@
+package fr.skynnotopia.skyadmin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyAdmin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
